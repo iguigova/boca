3fd7af5e5055377c49eef026daf8c7b3905e3ad1
Ilka Guigova <iguigova@logonlabs.com>
2021-03-11 09:40:30 -0800

    react - fix for broken tick-tock example

--- react/src/tick_tock.js
+++ react/src/tick_tock.js
@@ -9,12 +9,11 @@
 // }
 
 function Clock(props){
-    return (
-            <div>
+    return
+        <div>
             <h1>Hello, world!</h1>
             <h2>It is {props.date.toLocaleTimeString()}.</h2>
-            </div>
-    };
+        </div>;
 }
 
 function tick(){
