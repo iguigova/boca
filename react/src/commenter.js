71fe3c70bd342b6097231915d5be2ecb640ba903
Ilka Guigova <iguigova@logonlabs.com>
2021-05-06 09:27:34 -0700

    ui - added OnEnter handler

--- react/src/commenter.js
+++ react/src/commenter.js
@@ -1,13 +1,24 @@
 // https://dmitripavlutin.com/controlled-inputs-using-react-hooks/
 // https://www.robinwieruch.de/react-add-item-to-list
+
 function Commenter({comment, onChange, onSubmit}){
+
+    //https://reactjs.org/docs/events.html
+    //https://reactjs.org/docs/handling-events.html
+    function onEnter(e, handler) {
+        if (event.key === "Enter" || event.keyCode === 13) {
+            handler();
+        }
+    }
+
     return <div>
       <input 
         type="text" 
         name="commenter" 
         placeholder="Enter comment here..." 
-        value={comment}
-        onChange={onChange} 
+        value={comment || ""} // https://stackoverflow.com/questions/47012169/a-component-is-changing-an-uncontrolled-input-of-type-text-to-be-controlled-erro
+        onChange={onChange}
+        onKeyPress={(e) => onEnter(e, onSubmit)} // https://www.geeksforgeeks.org/how-to-use-onkeypress-event-in-reactjs/
       />
       <button type="button" onClick={onSubmit}>Submit</button>   
     </div>;
