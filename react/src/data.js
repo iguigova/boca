2a829faf9d095154da7bb5853372a5275c93f641
Ilka Guigova <iguigova@logonlabs.com>
2021-05-26 22:42:28 -0700

    ui - playing with images

--- react/src/data.js
+++ react/src/data.js
@@ -43,7 +43,9 @@ const users = [ {name: uuid(), avatarUrl: "https://picsum.photos/id/551/20"},
                 {name: uuid(), avatarUrl: "https://picsum.photos/id/552/20"}];
 
 const comments = [{uuid: uuid(), timestamp: new Date(), text: "Hello there", author: users[0]},
-                  {uuid: uuid(), timestamp: new Date(), text: "Are you looking for me?", author: users[1]}];
+                  {uuid: uuid(), timestamp: new Date(), text: "Are you looking for me?", author: users[1]},
+                  {uuid: uuid(), timestamp: new Date(), text: "https://www-1565g.bookeo.com/bookeo/cfile/415659FAAEL15FAD93BB55/1561140871799_3UCE3MT797A949W7Y9XAXL4JH9ULEU67_1000_666.jpg https://www.stopmotioncentral.com/wp-content/uploads/2018/10/Will-Vinton-Dies-at-Age-70-4thOct2018-302x180.jpg", author: users[0]}
+                 ];
 
 
 export { uuid, user, users, comments }
---
+++ react/src/lightbox.js
@@ -0,0 +1,29 @@
+// https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_modal_lightbox
+
+function Lightbox({urls, onClose}){
+    const [current, setCurrent] = React.useState(0);
+    
+    var pix = urls ? urls.map((url) => {return <img key={url} className="lightbox-pic" src={url} alt={url} />}) : null;
+    var index = urls ? urls.map((url) => { return <img key={url} className="lightbox-index-pic" src={url}/>}) : null;
+
+    var pic = pix ? pix[current] : null;
+    
+    function plusDivs(){
+    }
+    
+    return (
+      urls ?   
+      <div className="lightbox">
+        <span className="lightbox-close" onClick={onClose}>&times;</span>
+        {pic}
+        <button className="lightbox-left" onClick="plusDivs(-1)">&#10094;</button>
+        <button className="lightbox-right" onClick="plusDivs(1)">&#10095;</button>
+        <div className="lightbox-index">
+            {index}
+        </div>
+      </div>
+      : null
+    );    
+}
+
+export { Lightbox }
