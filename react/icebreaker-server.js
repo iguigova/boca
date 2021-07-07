1169b150406c6aae7041771dba62ea64a831b23d
Ilka Guigova <iguigova@logonlabs.com>
2021-07-07 00:27:11 -0700

    ui - added defaulting request to index.html (until the servers are swapped)

--- react/icebreaker-server.js
+++ react/icebreaker-server.js
@@ -15,11 +15,11 @@ var contentTypesByExtension = {
 };
 
 http.createServer(function (request, response) {
-    try {
+    try {     
         var requestUrl = url.parse(request.url)
-
+        
         // need to use path.normalize so people can't access directories underneath baseDirectory
-        var fsPath = baseDirectory+path.normalize(requestUrl.pathname)
+        var fsPath = baseDirectory+path.normalize(requestUrl.pathname === '/' ? '/index.html' : requestUrl.pathname)
 
         var fileStream = fs.createReadStream(fsPath)
         fileStream.pipe(response)
