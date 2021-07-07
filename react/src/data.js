673215f4c2cf9e3d9656897fdbddcf655f7462de
Ilka Guigova <iguigova@logonlabs.com>
2021-07-07 11:45:09 -0700

    ui - updated for safari on iphone

--- react/src/data.js
+++ react/src/data.js
@@ -2,11 +2,11 @@ import { uuid, hash, cookie } from '/dist/utils.js'
 
 function user(){
     var id = uuid();
-    return cookie("user", { name: id, avatarUrl: "https://picsum.photos/id/" + hash(id, 1084)  + "/20" });
+    return cookie("user", { name: id, avatarUrl: "https://picsum.photos/id/" + hash(id, 1084)  + "/24" });
 }
 
-const users = [ {name: uuid(), avatarUrl: "https://picsum.photos/id/551/20"},
-                {name: uuid(), avatarUrl: "https://picsum.photos/id/552/20"}];
+const users = [ {name: uuid(), avatarUrl: "https://picsum.photos/id/551/24"},
+                {name: uuid(), avatarUrl: "https://picsum.photos/id/552/24"}];
 
 const comments = [{uuid: uuid(), timestamp: new Date(), text: "Hello there", author: users[0]},
                   {uuid: uuid(), timestamp: new Date(), text: "Are you looking for me?", author: users[1]},
