158214eed956086c8a9bdd8c7c3eb87b14562c61
Ilka Guigova <iguigova@logonlabs.com>
2022-11-27 17:46:59 -0800

    enable message history

--- server/app/src/icebreaker/routes/websockets.clj
+++ server/app/src/icebreaker/routes/websockets.clj
@@ -4,18 +4,31 @@
             ))
 
 (defonce channels (atom {}))
+(defonce messages (atom {}))
 
 (defn connect! [pathname channel]
   (println "channel open: " channel " at " pathname)
-  (if (contains? @channels pathname)
-    (swap! channels update pathname conj channel)
-    (swap! channels assoc pathname #{channel})))
+  (store! channels pathname channel)
+  (sync-messages pathname channel))
 
 (defn disconnect! [pathname channel status]
   (println "channel closed:" status " at " pathname)
   (swap! channels #(remove #{channel} (pathname %)))) ;; swap! channels disj channel
 
-(defn notify-clients [pathname msg]
+(defn record! [pathname msg]
+  (store! messages pathname msg))
+
+(defn store! [store pathname obj]
+  (if (contains? @store pathname)
+    (swap! store update pathname conj obj)
+    (swap! store assoc pathname #{obj}))  )
+
+(defn sync-messages [pathname channel]
+  (doseq [msg (pathname @messages)]
+    (srvr/send! channel msg)))
+
+(defn notify-channels [pathname msg]
+  (record! pathname msg)
   (doseq [channel (pathname @channels)]
     (srvr/send! channel msg)))
 
@@ -24,7 +37,7 @@
     (let [pathname (keyword (:pathname (:params request)))]
          (connect! pathname channel)
          (srvr/on-close channel (partial disconnect! pathname channel))
-         (srvr/on-receive channel #(notify-clients pathname %)))))
+         (srvr/on-receive channel #(notify-channels pathname %)))))
 
 (defroutes websocket-routes
   (GET "/ws" request (ws-handler request)))
