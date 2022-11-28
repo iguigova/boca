5b5438862fdbb7a1bdf1508a57bb0c3b75076b6c
Ilka Guigova <iguigova@logonlabs.com>
2022-11-27 17:05:53 -0800

    enable pathname channels

--- server/app/src/icebreaker/routes/websockets.clj
+++ server/app/src/icebreaker/routes/websockets.clj
@@ -3,27 +3,28 @@
             [org.httpkit.server :as srvr] ;; :refer [send! with-channel on-close on-receive]]
             ))
 
-(defonce channels (atom #{}))
+(defonce channels (atom {}))
 
-(defn connect! [channel]
-  (println "channel open:" channel)
-  (swap! channels conj channel))
+(defn connect! [pathname channel]
+  (println "channel open: " channel " at " pathname)
+  (if (contains? @channels pathname)
+    (swap! channels update pathname conj channel)
+    (swap! channels assoc pathname #{channel})))
 
-(defn disconnect! [channel status]
-  (println "channel closed:" status)
-  (swap! channels #(remove #{channel} %))) ;; swap! channels disj channel
+(defn disconnect! [pathname channel status]
+  (println "channel closed:" status " at " pathname)
+  (swap! channels #(remove #{channel} (pathname %)))) ;; swap! channels disj channel
 
-(defn notify-clients [request msg]
-  ;; (println request)
-  (println (:pathname (:params request)))
-  (doseq [channel @channels]
+(defn notify-clients [pathname msg]
+  (doseq [channel (pathname @channels)]
     (srvr/send! channel msg)))
 
 (defn ws-handler [request]
   (srvr/with-channel request channel
-    (connect! channel)
-    (srvr/on-close channel (partial disconnect! channel))
-    (srvr/on-receive channel #(notify-clients request %))))
+    (let [pathname (keyword (:pathname (:params request)))]
+         (connect! pathname channel)
+         (srvr/on-close channel (partial disconnect! pathname channel))
+         (srvr/on-receive channel #(notify-clients pathname %)))))
 
 (defroutes websocket-routes
   (GET "/ws" request (ws-handler request)))
