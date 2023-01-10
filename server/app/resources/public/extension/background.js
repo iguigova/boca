e1e000216fcbc6935185c66a5752d22983f7a344
Ilka Guigova <iguigova@logonlabs.com>
2023-01-10 14:17:33 -0800

    Extension support for multiple boca tabs still without duplicates

--- server/app/resources/public/extension/background.js
+++ server/app/resources/public/extension/background.js
@@ -20,27 +20,35 @@
 //   return tab;
 // }
 
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0);
+
+const bocaHash = s => "boca" + hashCode(s);
+const bocaUrl = s => "https://bocado.news/boca/" + s;
+
 // https://stackoverflow.com/questions/71918972/in-a-chrome-extension-detect-tab-already-open-and-switch-to-it-otherwise-crea
 // The solution requires adding a Javascript Promise, a call to chrome.tabs.get(), and chrome.tabs.highlight(), using chrome.storage to store the tab.id, along with some error handling on chrome.runtime.lastError to your existing code.
-chrome.action.onClicked.addListener(function(tab){
-    console.log(tab);
-    
-    new Promise((resolve) => {
-        chrome.storage.local.get('UITabID', function(oSetting){
-            resolve(oSetting.UITabID || 0);
+const openTab = (key, url, index) => {
+    new Promise((tabId) => {
+        chrome.storage.local.get(key, function(tabs){
+            tabId(tabs[key] || 0);
         });
     })
-    .then((eTabID) => {
-        chrome.tabs.get(eTabID, function(eTab){
+    .then((tabId) => {
+        chrome.tabs.get(tabId, function(result){
             if (chrome.runtime.lastError) {
-                chrome.tabs.create({url: "https://bocado.news/boca/" + tab.url, index: tab.index + 1, active: true }, function(nTab){
-                    chrome.storage.local.set({UITabID: nTab.id});
+                chrome.tabs.create({url: url, index: index, active: true }, function(newTab){
+                    chrome.storage.local.set({ [key]: newTab.id }); // https://stackoverflow.com/questions/11692699/chrome-storage-local-set-using-a-variable-key-name
                 });
             } else {
-                chrome.tabs.highlight({tabs: eTab.index});
+                chrome.tabs.highlight({tabs: result.index});
             }
         });
     });
+};
+
+chrome.action.onClicked.addListener(function(tab){
+    openTab(bocaHash(tab.url), bocaUrl(tab.url), tab.index + 1);
 });
 
 // chrome.action.onClicked.addListener((tab) => {
